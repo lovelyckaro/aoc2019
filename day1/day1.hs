aaec1e7d0722ec36e57936a7d70de44d3b3b3f7e
Love Lyckaro <love.lyckaro@hotmail.se>
2019-12-02 15:37:26 +0100

    Add day1 code

---
+++ day1/day1.hs
@@ -0,0 +1,31 @@
+module Day1 where
+
+type Mass = Integer
+type Fuel = Integer
+
+calcFuel :: Mass -> Fuel
+calcFuel moduleMass = moduleFuel + fuelFuel moduleFuel
+  where 
+        moduleFuel :: Fuel
+        moduleFuel = massToFuel moduleMass
+
+massToFuel :: Mass -> Fuel
+massToFuel m = m `div` 3 - 2
+
+fuelFuel :: Fuel -> Fuel
+fuelFuel m | fuelMass <= 0 = 0
+           | otherwise    = fuelFuel fuelMass + fuelMass
+              where fuelMass = massToFuel m
+
+
+getInput :: FilePath -> IO [Mass]
+getInput fp = do
+  file <- readFile fp
+  return (map read $ filter (/= "") $ lines file)
+
+main :: IO()
+main = do
+  input <- getInput "input1.txt"
+  let fuel = sum . (map calcFuel) $ input
+  print fuel
+
